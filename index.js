4123c3e6303b1ae945447cbc00b2b9f163dcbeb9
MichiyukiAbe <MichiyukiAbe@MichiyukiAbe-no-MacBook-Pro.local>
2018-02-28 19:27:54 +0900

    add splitter option

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-nth-list', function (opts) {
+    var splitter = opts.splitter ? opts.splitter : " ";
     return function (css, result) {
         css.replaceValues(/nth\((.+),\s*([0-9]+)\)/, {fast: 'nth('}, function (string) {
             var match = string.match(/nth\((.+),\s*([0-9]+)/);
-            var list = match[1].split(" ");
+            var list = match[1].split(splitter);
             var index = match[2];
 
             return list[index];
